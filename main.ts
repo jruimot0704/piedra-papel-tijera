109ecd06379690037a98f70516798271803aa49a
jruimot0704 <122780882+jruimot0704@users.noreply.github.com>
2023-01-23 10:07:30 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,32 @@
-basic.forever(function () {
-	
+let random = 0
+input.onButtonPressed(Button.A, function () {
+    random = randint(1, 3)
+    basic.pause(1000)
+    if (random == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+    if (random == 2) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    }
+    if (random == 3) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
 })
